9eee9d7b58c0d1cebf5cfe881837875b49ede84f
Christian <kalonjic34@gmail.com>
2025-06-17 16:29:34 +0200

    Refactor todo list functionality to use match-case for user actions

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-user_prompt = "Enter a todo: "
-
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    print(todo.title())
-    todos.append(todo)
-    print(todos)
+    user_action = input("Type add, show or exit: ")
+    user_action = user_action.strip()
+    
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for item in todos:
+                print(item)
+        case 'exit':
+            break
+print("Bye!")
