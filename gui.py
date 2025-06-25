dbc4eefc95e832fe8512ad106150c72f5f8b1e84
Christian <kalonjic34@gmail.com>
2025-06-25 17:27:27 +0200

    Refactor GUI layout for improved user experience

--- gui.py
+++ gui.py
@@ -2,9 +2,29 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo",key = 'todo')
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+edit_button = sg.Button("Edit")
+
+window = sg.Window('My To-Do App', 
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   font = ("Helvetica",20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+            
+
 window.close()
